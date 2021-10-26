cca91138e4180bb252b0876a5869c6d425317ed2
ben <ben@frshwtr.uk>
2021-10-26 23:03:31 +0100

    feature: introduce display

---
+++ libs.h
@@ -0,0 +1,20 @@
+//
+// Created by ben on 26/10/2021.
+//
+
+#ifndef GAME_ENGINE_LIBS_H
+#define GAME_ENGINE_LIBS_H
+#include <iostream>
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
+#include <SOIL2.h>
+#endif //GAME_ENGINE_LIBS_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,49 @@
-#include <iostream>
+#include "libs.h"
+
+void framebuffer_resize_callback(GLFWwindow *window, int frameBufferWidth, int frameBufferHeight) {
+    glViewport(0, 0, frameBufferWidth, frameBufferWidth);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+
+    const int WINDOW_WIDTH = 640;
+    const int WINDOW_HEIGHT = 480;
+    int frameBufferWidth = 0;
+    int frameBufferHeight = 0;
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+
+    GLFWwindow *window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game Engine", NULL, NULL);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Something went wrong lol" << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(0.f, 0.f, 0.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glFlush();
+    }
+
+    glfwTerminate();
     return 0;
 }
