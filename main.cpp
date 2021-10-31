757573eb6ef62ded5d982be194304adaa6e5c01c
ben <ben@frshwtr.uk>
2021-10-31 10:20:43 +0000

    feature: fix triangle

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "libs.h"
 
 Vertex vertices[] = {
-        glm::vec3(-0.5f, 0.5f, 0.f), glm::vec3(1.f, 0.f, 0.f), glm::vec2(0.f, 1.f),
+        glm::vec3(0.f, 0.5f, 0.f), glm::vec3(1.f, 0.f, 0.f), glm::vec2(0.f, 1.f),
         glm::vec3(-0.5f, -0.5f, 0.f), glm::vec3(0.f, 1.f, 0.f), glm::vec2(0.f, 0.f),
         glm::vec3(0.5f, -0.5f, 0.f), glm::vec3(0.f, 0.f, 1.f), glm::vec2(1.f, 0.f)
 };
